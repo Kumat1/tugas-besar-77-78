e375a1f2c4c33d215a31bf9f9d01193ac4a254fa
WILLY <willymardianto97@gmail.com>
2017-06-01 09:30:16 +0700

    Pada commit minggu ini, kami membuat multiple label dalam JScrollPanel agar dapat menampilkan seluruh data yang ada dalam database

--- src/Tubes.java
+++ src/Tubes.java
@@ -1,22 +1,28 @@
+package file;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Tubes extends JFrame {
-    public JPanel panel, panelnew, paneltiga;
+    public JPanel panel, panelnew, panelawal, scroll;
     public JTextArea areanew;
     public JTextField fieldjudul, fieldtanggal, fieldkategori;
     public String Sjudul, Stanggal, Skategori, Sisi;
+    public JScrollPane listItem;
+    public Date tanggal;
+    public JLabel list;
+    public BoxLayout boxLayout;
     private JMenuBar menu;
     private JMenu fileMenu;
     private JMenuItem close, New, save, home;
 
 
-    public Tubes(){
+    public Tubes() throws SQLException {
         super("Agenda");
 
         try {
@@ -49,9 +55,45 @@ public class Tubes extends JFrame {
         CardLayout cards = new CardLayout();
         JPanel cardPanel = new JPanel();
 
+        //panel semua
+//        JPanel panel = (JPanel) getContentPane();
+//        panel.setLayout(null);
+
+        panel = new JPanel(new BorderLayout());
+        panel.add(cardPanel, BorderLayout.CENTER);
+
+        //panelscroll
+        scroll = new JPanel();
+
+        //scrollpane
+//        listItem = new JScrollPane();
+//        listItem.setPreferredSize(new Dimension(150,150));
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "");
+        String panggil = "SELECT * FROM isi";
+        Statement sta = connection.createStatement();
+        ResultSet rse = sta.executeQuery(panggil);
+
+        while(rse.next()){
+            list = new JLabel(rse.getString(1));
+            list.setPreferredSize(new Dimension(100,100));
+            list.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            scroll.add(list);
+            scroll.add(Box.createRigidArea(new Dimension(10, 5)));
+        }
+        boxLayout = new BoxLayout(scroll, BoxLayout.Y_AXIS);
+        scroll.setLayout(boxLayout);
+        listItem = new JScrollPane(scroll);
+        listItem.setPreferredSize(new Dimension(150,150));
+
+        panel.add(listItem, BorderLayout.WEST);
+
+        connection.close();
+
+
         //panel awal
-        panel = new JPanel();
-        panel.setBackground(Color.GREEN);
+        panelawal = new JPanel();
+        panelawal.setBackground(Color.BLACK);
 
         //panel new
         panelnew = new JPanel(new GridBagLayout());
@@ -61,19 +103,16 @@ public class Tubes extends JFrame {
         fieldkategori = new JTextField(30);
 
         areanew = new JTextArea();
-
-        //panel yang belum kepakek
-        paneltiga = new JPanel();
-        paneltiga.setBackground(Color.BLACK);
+        areanew.setSize(450,150);
 
         cardPanel.setLayout(cards);
         cards.show(cardPanel, "CardLayout");
 
-        cardPanel.add(panel, "Card 1");
+        cardPanel.add(panelawal, "Card 1");
         cardPanel.add(areanew, "Card 2");
-        cardPanel.add(paneltiga, "Card 3");
 
-        getContentPane().add(cardPanel);
+        getContentPane().add(panel);
+
 
         New.addActionListener(new ActionListener() {
             @Override
@@ -91,10 +130,6 @@ public class Tubes extends JFrame {
                 panelnew.add(judul,c);
 
                 c.gridy = 1;
-                JLabel tanggal = new JLabel("Tanggal : ");
-                panelnew.add(tanggal,c);
-
-                c.gridy = 2;
                 JLabel kategori = new JLabel("Kategori : ");
                 panelnew.add(kategori,c);
 
@@ -104,20 +139,16 @@ public class Tubes extends JFrame {
                 panelnew.add(fieldjudul,c);
 
                 c.gridy = 1;
-                panelnew.add(fieldtanggal,c);
-
-                c.gridy = 2;
                 panelnew.add(fieldkategori,c);
 
                 int result = JOptionPane.showConfirmDialog(null, panelnew, "Agenda", JOptionPane.OK_CANCEL_OPTION);
 
                 if (result == JOptionPane.OK_OPTION) {
                     Sjudul = fieldjudul.getText();
-                    Stanggal = fieldtanggal.getText();
                     Skategori = fieldkategori.getText();
                 }
 
-                panel.setVisible(false);
+                panelawal.setVisible(false);
                 areanew.setVisible(true);
             }
         });
@@ -126,8 +157,15 @@ public class Tubes extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Sisi = areanew.getText();
+
+                //tanggal save/update terakhir
+                tanggal = new Date( );
+                SimpleDateFormat ft = new SimpleDateFormat ("yyyy/MM/dd");
+
+                Stanggal = ft.format(tanggal);
+
                 try {
-                    insert();
+                    check();
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -139,7 +177,6 @@ public class Tubes extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 panel.setVisible(true);
                 //panelnew.setVisible(true);
-                paneltiga.setVisible(false);
             }
         });
 
@@ -156,8 +193,41 @@ public class Tubes extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
         Tubes agenda = new Tubes();
+    }
+
+    public void check() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "");
+
+        String panggil = "SELECT * FROM isi";
+        Statement sta = connection.createStatement();
+        ResultSet rse = sta.executeQuery(panggil);
+
+        while(rse.next()){
+          if(rse.getString("title").equals(Sjudul)){
+              update();
+              connection.close();
+                return;
+          }
+        }
+        insert();
+        connection.close();
+    }
+
+    public void update() throws SQLException {
+        Connection connection = Koneksi.getConnection();
+
+        PreparedStatement ps = connection.prepareStatement
+                ("UPDATE isi SET tanggal=?, kategori=?, isi=? WHERE title=? ");
+
+        ps.setString(1, Stanggal);
+        ps.setString(2, Skategori);
+        ps.setString(3, Sisi);
+        ps.setString(4, Sjudul);
+
+        ps.executeUpdate();
+        connection.close();
     }
 
     public void insert() throws SQLException {
